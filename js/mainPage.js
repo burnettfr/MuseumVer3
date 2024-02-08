b67ffab4520ba0e7a76b79aea9980536d182be11
burnettfr <burnettfr@gmail.com>
2024-02-08 06:17:42 -0500

    Create cotton quiz

--- js/mainPage.js
+++ js/mainPage.js
@@ -1,16 +1,22 @@
 document.querySelector("#button1").addEventListener("click", clickButton1);
 document.querySelector("#button2").addEventListener("click", clickButton2);
+document.querySelector("#button3").addEventListener("click", clickButton3);
 
 function clickButton1(){
-    console.log("Button 1 Clicked");
+    // console.log("Button 1 Clicked");
     window.location.href = 'file:///C:/Gronkle/Engineering/Web%20Development/Museum/Ver3/music.html'
-    // window.location.href = 'file:///C:/burnett/MuseumVer2/music.html'
+    // window.location.href = 'file:///C:/burnett/MuseumVer3/music.html'
 
 }
 
 function clickButton2(){
-    console.log("Button 1 Clicked");
     window.location.href = 'file:///C:/Gronkle/Engineering/Web%20Development/Museum/Ver3/reconstruction.html'
-    // window.location.href = 'file:///C:/burnett/MuseumVer2/reconstruction.html'
+    // window.location.href = 'file:///C:/burnett/MuseumVer3/reconstruction.html'
+
+}
+
+function clickButton3(){
+    window.location.href = 'file:///C:/Gronkle/Engineering/Web%20Development/Museum/Ver3/cotton.html'
+    // window.location.href = 'file:///C:/burnett/MuseumVer3/cotton.html'
 
 }
